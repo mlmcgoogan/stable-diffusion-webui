51f88593e5da89bf8556cdbe8a2d1c45984b6684
Matthew McGoogan <mlmcgoogan@gmail.com>
2022-11-27 16:39:03 +0000

    Adding helper script for launch with desired options

---
+++ sb_launch.sh
@@ -0,0 +1,8 @@
+export PYTHONUNBUFFERED=1
+SD_DEVICE=$1
+MODEL_PATH=$2
+SD_PORT=$((6500+$SD_DEVICE))
+LOG_FILENAME="sb_${SD_DEVICE}_${SD_PORT}.log"
+echo "RUNNING python launch.py --xformers --listen --device-id $SD_DEVICE --port $SD_PORT --api --enable-insecure-extension-access --vae-path $MODEL_PATH/improved_faces.vae.pt &> ./$LOG_FILENAME &"
+python launch.py --xformers --listen --device-id $SD_DEVICE --port $SD_PORT --api --enable-insecure-extension-access --vae-path $MODEL_PATH/improved_faces.vae.pt &> $LOG_FILENAME &
+echo "Started with PID $!"
