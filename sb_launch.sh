bc1bbde4f0a3781856b00a37aecab458995e408b
Matthew McGoogan <mlmcgoogan@gmail.com>
2022-12-10 16:31:31 +0000

    Updating launch helper to support re-launching.

--- sb_launch.sh
+++ sb_launch.sh
@@ -1,9 +1,19 @@
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate auto1111
-export PYTHONUNBUFFERED=1
+
 SD_DEVICE=$1
 SD_PORT=$((6500+$SD_DEVICE))
 LOG_FILENAME="sb_${SD_DEVICE}_${SD_PORT}.log"
-echo "RUNNING python launch.py --xformers --listen --device-id $SD_DEVICE --port $SD_PORT --api --enable-insecure-extension-access &> ./$LOG_FILENAME &"
+
+PID=$(cat sb_${SD_DEVICE}.pid)
+if ps -p $PID > /dev/null
+then
+   echo "Already running. Killing $PID"
+   kill $PID
+fi
+
+export PYTHONUNBUFFERED=1
+echo "RUNNING python launch.py --xformers --listen --device-id $SD_DEVICE --port $SD_PORT --api --enable-insecure-extension-access --disable-safe-unpickle  &> ./$LOG_FILENAME &"
 python launch.py --xformers --listen --device-id $SD_DEVICE --port $SD_PORT --api --enable-insecure-extension-access --disable-safe-unpickle &> $LOG_FILENAME &
-echo "Started with PID $!"
+echo "Launched with PID $!"
+echo $! > sb_${SD_DEVICE}.pid
